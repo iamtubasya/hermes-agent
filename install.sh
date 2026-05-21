380a2642ccdbaa3aec045a2c6e6d329fa78cf257
I'M TUBASYA <160491335+iamtubasya@users.noreply.github.com>
2026-05-21 11:37:27 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 apt update && apt upgrade -y
 apt install curl git nano -y
 apt install nodejs npm python3 -y
+export PATH="/root/.local/bin:$PATH"
 
 curl -fsSL https://raw.githubusercontent.com/NousResearch/hermes-agent/main/scripts/install.sh | bash
