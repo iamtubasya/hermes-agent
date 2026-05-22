5d004e62b6d6ee702eb6f67ca20b8ec8378cb551
I'M TUBASYA <160491335+iamtubasya@users.noreply.github.com>
2026-05-22 18:29:35 +0700

    Add files via upload

---
+++ chiperflux-debian.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pkg install openssl-tool -y
+openssl enc -d -aes-256-cbc -pbkdf2 -in chiper-debian.enc
---
+++ chiperflux-hermes.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+openssl enc -d -aes-256-cbc -pbkdf2 -in chiper-hermes.enc
